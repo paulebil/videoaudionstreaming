7f11e1e6b181fa1ba8345c9b869431d1aa9441bd
paulebil <ebilpaul06@gmail.com>
2026-05-30 23:58:47 +0300

    feat: added the media routers to the main app.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
+from media.routers import media_asset_router
 
 app = FastAPI()
+
+app.include_router(media_asset_router)
+
+add_pagination(app)
+
 
 @app.get("/")
 async def get():
