bf73a4afd84847bcc0eb1e4a5d355e1b23c48588
paulebil <ebilpaul06@gmail.com>
2026-06-03 15:03:00 +0300

    feat: added corss to the backend server.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
+from fastapi.middleware.cors import CORSMiddleware
 
 from media.routers import media_asset_router
 
@@ -8,6 +9,18 @@ app = FastAPI()
 app.include_router(media_asset_router)
 
 add_pagination(app)
+
+origins = [
+    "http://localhost:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
